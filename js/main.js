7960c586594e0f30491d413b5d5d86b3497bd93d
joseph <josephjohndemarco@gmail.com>
2020-10-30 21:29:39 -0400

    Add icon to button. Add styling. Add more skeleton.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let ipAddress;
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
